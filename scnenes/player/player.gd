daeade211eb5cfbcddc27f84786a63be9e7a1be6
Jacky <jchoi53@mail.ccsf.edu>
2023-12-06 12:18:25 -0800

    add player movement

---
+++ scnenes/player/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const MAX_SPEED = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity = direction * MAX_SPEED #200 pixels / sec in direction we press
+	move_and_slide() #tells body to move and creates smooth collisions
+
+
+func get_movement_vector():
+	var movement_vector = Vector2.ZERO #sets to (0,0)
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left") #if right is pressed x = 1 and if both are pressed we get 0.
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up") #since down is positive and up is negative down comes first
+	return Vector2(x_movement, y_movement)
