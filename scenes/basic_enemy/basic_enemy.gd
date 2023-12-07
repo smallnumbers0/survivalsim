8e40ddb01f296a6340104dc9a232d669d605a25b
Jacky <jchoi53@mail.ccsf.edu>
2023-12-07 12:56:11 -0800

    add sword attacks enemy

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -5,7 +5,7 @@ const MAX_SPEED = 75
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Area2D.area_entered.connect(on_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,4 +19,7 @@ func get_direction_to_player():
 	var player_node = get_tree().get_first_node_in_group("player") as Node2D
 	if player_node != null:
 		return (player_node.global_position-global_position).normalized()
-		return Vector2.ZERO
+	return Vector2.ZERO
+
+func on_area_entered(other_area: Area2D):
+	queue_free()
